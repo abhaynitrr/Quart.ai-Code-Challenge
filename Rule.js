13dd01a522b70dc71c392546ac8711b71dc4d23f
abhaynitrr <41701130+abhaynitrr@users.noreply.github.com>
2018-07-25 23:49:13 +0530

    Files added

---
+++ Rule.js
@@ -0,0 +1,6 @@
+rules = [
+    "ATL1 value should rise above 240.00",
+    "ATL10 value should not be HIGH",
+    "ATL5 value should not be HIGH",
+    "ATL9 value should not be greater than 2017-08-13 22:40:10"
+];
